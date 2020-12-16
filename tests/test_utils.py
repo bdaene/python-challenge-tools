5212c3f29e601907adfda1226bbc2cb9160fe41b
bdaene <benoitdaene@gmail.com>
2020-12-16 09:09:02 +0100

    Add timeit decorator in utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from math import comb
 
 import pytest
 
-from utils import comb_mod, gen_pythagorean_triples
+from utils import comb_mod, gen_pythagorean_triples, timeit
 
 
 @pytest.mark.parametrize("n, r, m", [
@@ -23,3 +23,11 @@ def test_comb_mod(n, r, m):
 ])
 def test_gen_pythagorean_triples(triple, primitive):
     assert triple in set(gen_pythagorean_triples(triple[0], primitive))
+
+
+@pytest.mark.parametrize("func, args, kwargs, result", [
+    (lambda: 0, (), {}, 0),
+    (lambda a, b, c=3: a + b + c, (1,), {'b': 2}, 6),
+])
+def test_timeit(func, args, kwargs, result):
+    assert timeit(func)(*args, **kwargs) == result
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 """Library containing tools that do not fit in specialized libraries"""
 
+from functools import wraps
 from math import comb, gcd
+from sys import stdout
+from time import perf_counter
 
 
 def comb_mod(n, r, mod):
@@ -15,7 +18,7 @@ def comb_mod(n, r, mod):
 
 
 def gen_pythagorean_triples(max_z, primitive=False):
-    """ Generate pythagorean triple (z,y,x) such that (z*z = y*y+x*x) and z>y>x>0 """
+    """Generate pythagorean triple (z,y,x) such that (z*z = y*y+x*x) and z>y>x>0"""
     for m in range(2, int(max_z ** 0.5) + 1):
         m2 = m * m
         for n in range(m % 2 + 1, min(int((max_z - m2) ** 0.5) + 1, m), 2):
@@ -29,3 +32,18 @@ def gen_pythagorean_triples(max_z, primitive=False):
                 else:
                     for k in range(1, max_z // z + 1):
                         yield z * k, y * k, x * k
+
+
+def timeit(func, file=stdout):
+    """Allows to time an execution while still getting the result. With a simple decorator."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        clock = perf_counter()
+        result = func(*args, **kwargs)
+        print(f"Execution of {func.__name__} took {(perf_counter() - clock) * 1000:.3f}ms.", file=file)
+        print(f"Result: {result}", file=file)
+        return result
+
+    wrapper.func = func
+    return wrapper
